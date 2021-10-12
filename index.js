d5519310d3e53852b585eabf8b7223801e7ef89b
simonachess <simonachess@gmail.com>
2021-10-12 21:42:19 +0300

    create file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const data = require('./lib/data.js');
 
-data.create('users', 'petras', {name: 'Petras', age:99});
+data.create('users', 'petras', { name: 'Petras', age: 99 }, (state, msg) => {
+    if (state) {
+        console.log(msg);
+    } else {
+        console.error(msg);
+    }
+
+    console.log('Sekantys zingsniai po bandymo sukurti faila....');
+});
