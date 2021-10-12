32070e72cd9e5885f34a3fa07f5284cada236f00
simonachess <simonachess@gmail.com>
2021-10-12 20:53:51 +0300

    absolute filepath

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const data = require('./lib/data.js');
 
-console.log(data);
-
 data.create('users', 'petras', {name: 'Petras', age:99});
-data.read('users', 'petras');
-data.update();
-data.delete();
-
-
-data.create('books', 'du-gaideliai', { text: 'asd'});
-data.read('books', 'du-gaideliai');
