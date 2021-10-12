a85f6fa4be0398f2917cd72715f3d897faf47e07
simonachess <simonachess@gmail.com>
2021-10-12 19:56:54 +0300

    funkciju dokumentavimas

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const data = require('./lib/data.js');
 
 console.log(data);
 
-data.create();
-data.read();
+data.create('users', 'petras', {name: 'Petras', age:99});
+data.read('users', 'petras');
 data.update();
 data.delete();
+
+
+data.create('books', 'du-gaideliai', { text: 'asd'});
+data.read('books', 'du-gaideliai');
