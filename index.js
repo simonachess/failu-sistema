f0574e47294732d3d799f8df22f88ce938ef5f7d
simonachess <simonachess@gmail.com>
2021-10-13 18:41:51 +0300

    read function

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const data = require('./lib/data.js');
 
-data.create('users', 'petras', { name: 'Petras', age: 99 }, (state, msg) => {
-    if (state) {
-        console.log(msg);
-    } else {
-        console.error(msg);
-    }
+// data.create('users', 'petras', { name: 'Petras', age: 99 }, (state, msg) => {
+// console.log(state, msg);
+//     console.log('Sekantys zingsniai po bandymo sukurti faila....');
+// });
 
-    console.log('Sekantys zingsniai po bandymo sukurti faila....');
+// data.create('books', 'du-gaideliai', { text: 'Baltus zirnius kule'}, (state, msg) => {
+//     console.log(state, msg);
+//     console.log('Sekantys zingsniai po bandymo sukurti faila....');
+// });
+
+data.read('users', 'petras', (err, content)=>{
+    const fileData = JSON.parse(content);
+    console.log(fileData.name);
+    console.log(fileData.age);
 });
