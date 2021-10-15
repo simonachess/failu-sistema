ba2434ee8b8baee2f6679039e2d23803a40ab1f0
simonachess <simonachess@gmail.com>
2021-10-15 17:53:34 +0300

    append to array

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
+const { appendToArray } = require('./lib/data.js');
 const data = require('./lib/data.js');
 
 const initialDataPetras = { name: 'Petras', age: 99 };
 
-data.create('users', 'petras', initialDataPetras, (err, msg) => {
-    console.log(err, msg);
-    console.log('Sekantys zingsniai po bandymo sukurti faila....');
-    data.read('users', 'petras', (err, content) => {
-        const fileData = JSON.parse(content);
-        console.log('READ 1', fileData);
+// data.create('books', 'Dievu miskas', { text: 'Amen'}, (err, msg) => {
+//     console.log(err, msg);
+//     console.log('Sekantys zingsniai po bandymo sukurti faila....');
+// });
 
-        data.update('users', 'petras', { ...initialDataPetras, favoriteColor: 'red' }, (err, msg) => {
-            console.log('UPDATE 1', err, msg);
+// data.create('books', 'Anyksciu silelis', { text: 'Zibintai zibintai'}, (err, msg) => {
+//     console.log(err, msg);
+//     console.log('Sekantys zingsniai po bandymo sukurti faila....');
+// });
 
-            //dar karta perskaitome 
-            data.read('users', 'petras', (err, content) => {
-                const fileData = JSON.parse(content);
-                console.log('READ 2', fileData);
 
-                data.delete('users', 'petras', (err, msg) => {
-                    console.log('DELETE 1', err, msg);
-                })
-            });
-        });
-    });
-});
+
+// data.create('users', 'petras', initialDataPetras, (err, msg) => {
+//     console.log(err, msg);
+//     console.log('Sekantys zingsniai po bandymo sukurti faila....');
+//     data.read('users', 'petras', (err, content) => {
+//         const fileData = JSON.parse(content);
+//         console.log('READ 1', fileData);
+
+//         data.update('users', 'petras', { ...initialDataPetras, favoriteColor: 'red' }, (err, msg) => {
+//             console.log('UPDATE 1', err, msg);
+
+//             //dar karta perskaitome 
+//             data.read('users', 'petras', (err, content) => {
+//                 const fileData = JSON.parse(content);
+//                 console.log('READ 2', fileData);
+
+//                 data.delete('users', 'petras', (err, msg) => {
+//                     console.log('DELETE 1', err, msg);
+//                 })
+//             });
+//         });
+//     });
+// });
 
 // data.create('books', 'du-gaideliai', { text: 'Baltus zirnius kule'}, (err, msg) => {
 //     console.log(err, msg);
@@ -33,4 +46,45 @@ data.create('users', 'petras', initialDataPetras, (err, msg) => {
 
 
 //prie Petro prideti megstama spalva
+
+// data.folderContent('books', (err, content) => {
+//     console.log(err);
+//     console.log(content);
+//     if (!err) {
+//         // eiti per failu sarasa ir kiekviena ju perskaityti
+//         for (const book of content) {
+//             data.read('books', book, (err, bookContent) => {
+//                 console.log(err, bookContent);
+//             })
+//         }
+//     }
+// });
+
+const pirmasKioskas = {
+    name: 'Pirmas kioskas',
+    size: 20,
+    price: 5000
+}
+
+const antrasKioskas = {
+    name: 'Antras kioskas',
+    size: 15,
+    price: 4000
+}
+
+data.appendToArray('shop', 'kioskas', pirmasKioskas, (err, content) => {
+    // tikimasi, jog sekmes atveju, grazinamas turinys
+    // bus atnaujinta failo turinio versija
+    // turesim matyti:
+    // [{"name":"Pirmas kioskas","size":20,"price":5000}]
+    console.log(err, content);
+    data.appendToArray('shop', 'kioskas', antrasKioskas, (err, content) => {
+        // tikimasi, jog sekmes atveju, grazinamas turinys
+        // bus atnaujinta failo turinio versija
+        // turesim matyti:
+        // [{"name":"Pirmas kioskas","size":20,"price":5000},{"name":"Antras kioskas","size":15,"price":4000}]
+        console.log(err, content);
+    })
+})
+
 
